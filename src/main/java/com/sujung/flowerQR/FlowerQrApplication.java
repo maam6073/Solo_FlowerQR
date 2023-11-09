018e559a90287d61db8853ab146d290fe7eb0392
maam6073 <maam10@naver.com>
2023-11-09 17:18:22 +0900

    init

---
+++ src/main/java/com/sujung/flowerQR/FlowerQrApplication.java
@@ -0,0 +1,13 @@
+package com.sujung.flowerQR;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowerQrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowerQrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sujung/flowerQR/FlowerQrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sujung.flowerQR;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowerQrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
